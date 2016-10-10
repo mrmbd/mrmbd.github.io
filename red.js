d8669366db20adbfc27dca0b8ce8d8fa79051bfc
mrmbd <mihirbharatdave@gmail.com>
2016-10-10 19:18:21 +0530

    Update red.js

--- red.js
+++ red.js
@@ -1,17 +1,33 @@
- A one place solution for all your advertising needs.
-Get the right people to connect with your product.
+														A one place solution for all your advertising needs .
+														  Get the right people to connect with your product.
+What we do?
 
-Advertising Platform
-We are one of the largest and the best Demand-Site Platforms(DSP) advertising platform for buying pop under traffic for your websites.
+			Advertising Platform
+We are one of the largest and the best Demand-Site advertising Platforms(DSP) for buying pop under traffic for your websites.
 
-Buy ADs
-we help you purchase, assess, target and optimize pop under Pay-Per View(PPV) ads bought from our third parties and filtered through our engines according to your needs.
+			Buy ADs
+we help you purchase, assess, target and optimize pop under Pay-Per View(PPV) ads bought from our third parties and filtered through our engines as per your needs.
 
 Targeting
-we use sophisticated and best algorithms that works with Real Time Bidding of ad networks so that you get the most effective and cost efficient service.
+we use sophisticated and best algorithms that works with Real Time Bidding of ad networks so that you get the most effective and cost efficient services.
 
-Filter
+Filters
 You get to target the audience of your choice ranging from different countries to different ages.While we on our part ensure that you get high quality traffic.
 
 Our Reach
-We buy large amount of traffic from all pop under ad networks like adcash, tonic, popcash, and many more.So that you get the lowest price per pop.
+We buy traffic from all pop under ad networks like adcash, tonic, popcash, and many more.So that you get wide array of networks to showcase your products.
+
+Our Assurance
+You can rely on our pop under networks , to get guranteed impressions on your website per click.---And your billing information is stored in secure and safe environment.
+
+Low Cost
+We buy huge amount of traffic from all the advertising networks and thus get them at cheaper rate than normal advertisors, which in turn helps you save money.
+
+User Friendly
+Our interface is interactive as well as easy to use, comprising of all the tools that you would require to build a strong ad campaign.And in case you have any problems, our support staff is always there to help you.
+
+Fast Access
+Buy from our pop under ad networks in real-time, with the help of our robust decisioning engines, Or Automate your campaign with our scheduling system.
+
+Reports
+Get detailed reports of your campaigns with our powerful Reporting capabilities, And our transparent business model. 
